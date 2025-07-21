e726d7d32012cb7c5ded4af7ff7cfaf1cc803bac
Vansh Dubey <vanshdubey101963@gmail.com>
2025-07-21 20:46:40 +0530

    complete nn with backprop

--- backend/ml/neuron.py
+++ backend/ml/neuron.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import fetch_openml
+from sklearn.datasets import load_digits
+from keras.api.models import Sequential
+from keras.api.layers import Dense
 
 class NeuralNetwork:
-    def __init__(self, max_iterations=10000):
+    def __init__(self, max_iterations=1000):
         self.W1, self.W2, self.W3 , self.B1 , self.B2 , self.B3 = None,None,None,None,None,None
-        self.__learning_rate = 0.01
+        self.__learning_rate = 0.05
         self.max_iterations = max_iterations
 
     def __softmax(self, z):
@@ -23,12 +25,12 @@ class NeuralNetwork:
     
     def initialize_params(self):
 
-        self.W1 = np.random.rand(32,784) * 0.01
-        self.B1 = np.random.rand(32,1) * 0.01
-        self.W2 = np.random.rand(32,32) * 0.01
-        self.B2 = np.random.rand(32,1) * 0.01
-        self.W3 = np.random.rand(10,32) * 0.01
-        self.B3 = np.random.rand(10,1) * 0.01
+        self.W1 = np.random.rand(32,64) 
+        self.B1 = np.zeros((32,1))  
+        self.W2 = np.random.rand(32,32)
+        self.B2 = np.zeros((32,1))
+        self.W3 = np.random.rand(10,32)
+        self.B3 = np.zeros((10,1))
 
     def forward_prop(self,X):
         
@@ -42,6 +44,8 @@ class NeuralNetwork:
         A3 = self.__softmax(Z3)
 
         mem = (Z1, A1, Z2, A2, Z3, A3)
+        
+
         return A3, mem
 
 
@@ -53,12 +57,10 @@ class NeuralNetwork:
         dZ3 = self.__dsoftmax(A3, Y)
         dW3 = dZ3 @ A2.T / m
         db3 = np.sum(dZ3, axis=1, keepdims=True) / m
-
         dA2 = self.W3.T @ dZ3
         dZ2 = dA2 * self.__dsigmoid(A2)
         dW2 = dZ2 @ A1.T / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
-
         dA1 = self.W2.T @ dZ2
         dZ1 = dA1 * self.__dsigmoid(A1)
         dW1 = dZ1 @ X.T / m
@@ -71,12 +73,10 @@ class NeuralNetwork:
         return one_hot_encoded.T
         
 
-    def fit(self,X,y):
+    def fit(self,X_r,y):
+        X_r = X_r.T.reshape(64,-1)
         onehot_y = self.__onehot(y)
-        X_r = np.reshape(X, (784,1000))
-
         self.initialize_params()
-
 
         for i in range(1,self.max_iterations + 1):
 
@@ -98,10 +98,19 @@ class NeuralNetwork:
         A3 , mem = self.forward_prop(X)
         return A3
 
-mnist = fetch_openml('mnist_784', as_frame=False)
+def one_hot(y):
+    one_hot_encoded = np.eye(10)[y]
+    return one_hot_encoded.T
 
-X = mnist.data[0:1000]
-Y = mnist.target[0:1000]
-new_Y = Y.astype(int)
+digits = load_digits()
+
+X = digits.data / 16
+Y = digits.target
+Y = Y.astype(int)
 nn = NeuralNetwork()
-nn.fit(X,new_Y)
+nn.fit(X,Y)
+prediction = nn.predict(X[2].T.reshape(64,-1))
+print(prediction)
+print(one_hot(Y[2]))
+print(one_hot(Y[2]).argmax())
+print(prediction.argmax())
